ba1522c7ea37a7f214f9dd8cbee0bb279de9dc8d
Mohamed Ibrahim <mohamedhassen.ibrahim@mail.utoronto.ca>
2024-11-01 18:52:32 -0400

    chore: created sample messages

--- app.js
+++ app.js
@@ -10,8 +10,21 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(assetsPath));
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { messages: messages });
 })
 
 const PORT = process.env.PORT || 3000;
